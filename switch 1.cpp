a699811e573f575b8e0b15e4296aef98a646fded
abhishekdhakar475 <abhishekdhakar475@gmail.com>
2024-12-04 04:36:51 -0800

    Add files via upload

---
+++ switch 1.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enetr case no:");
+	scanf("%d",&a);
+	
+	switch(a){
+	
+	case 1:
+		int b;
+		printf("enter no:");
+		scanf("%d",&b);
+		int c;
+		printf("enetr2 no");
+		scanf("%d",&c);
+		
+		printf("%d",b+c);
+		break;
+		
+	case 2:
+	int e;
+	printf("enter no:");
+	scanf("%d",&e);
+	int f;
+	printf("enetr2 no");
+	scanf("%d",&f);
+		
+		int g=e-f;
+		printf("%d",g);
+		break;
+		}
+}
